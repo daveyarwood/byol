d4812c7218509426f13f047e7c5e76d4da52e0ac
Dave Yarwood <dave.yarwood@gmail.com>
2015-12-14 09:51:21 -0500

    print, error fns

--- strings.c
+++ strings.c
@@ -1022,6 +1022,27 @@ lval* builtin_load_file(lenv* e, lval* a) {
   }
 }
 
+lval* builtin_print(lenv* e, lval* a) {
+  for (int i = 0; i < a->count; i++) {
+    lval_print(a->cell[i]);
+    putchar(' ');
+  }
+
+  putchar('\n');
+  lval_del(a);
+
+  return lval_sexpr();
+}
+
+lval* builtin_error(lenv* e, lval* a) {
+  LASSERT_NUM("error", a, 1);
+  LASSERT_TYPE("error", a, 0, LVAL_STR);
+
+  lval* err = lval_err(a->cell[0]->str);
+  lval_del(a);
+  return err;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 /*
@@ -1290,10 +1311,14 @@ void lenv_add_builtins(lenv* e) {
   lenv_add_builtin(e, "!", builtin_not);
   lenv_add_builtin(e, "not", builtin_not); // alias
 
-  /* Variable functions */
+  /* Variable/environment functions */
   lenv_add_builtin(e, "def", builtin_def);
   lenv_add_builtin(e, "=", builtin_put);
   lenv_add_builtin(e, "print-env", builtin_print_env);
+
+  lenv_add_builtin(e, "load-file", builtin_load_file);
+  lenv_add_builtin(e, "error", builtin_error);
+  lenv_add_builtin(e, "print", builtin_print);
 
   /* Function functions */
   lenv_add_builtin(e, "\\", builtin_lambda);
